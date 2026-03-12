937db9b846e66036d9d2da1f4ab18d2c48c8832e
MourtallaGueye <mourtallagueye03@gmail.com>
2026-03-12 17:13:59 +0000

    fix: server.js routes + aliases articleController

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,13 @@
-
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
+const cors    = require('cors')
+const routes  = require('./routes/index')
+
 const app = express()
 
 app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Serveur Mourtalla OK !' })
-})
+app.use('/api', routes)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Serveur démarré sur http://localhost:5000')
